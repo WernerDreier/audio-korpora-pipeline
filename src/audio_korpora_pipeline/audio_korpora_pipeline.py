c4520d3bb0261f53e34cdc04ef3e59357f977f12
Werner Dreier <werner.dreier@yourscorez.de>
2019-08-18 12:47:33 +0200

    codeformatter added

--- src/audio_korpora_pipeline/audio_korpora_pipeline.py
+++ src/audio_korpora_pipeline/audio_korpora_pipeline.py
@@ -1,41 +1,38 @@
 # -*- coding: utf-8 -*-
 
 import configparser
-import sys
-import os
 import logging
+import os
+
 
 class Audiokorporapipeline:
 
-    def __init__(self, config_path):
-        """
-            parse config, setup logging
-        """
+  def __init__(self, config_path):
+    """
+        parse config, setup logging
+    """
 
-        self.__load_config(config_path)
-        self.logger = logging.getLogger(__name__)
+    self.__load_config(config_path)
+    self.logger = logging.getLogger(__name__)
 
-    def __load_config(self, config_path):
-        """
-            Parse config file
+  def __load_config(self, config_path):
+    """
+        Parse config file
 
-        """
+    """
 
-        self.config = configparser.RawConfigParser()
-        try:
-            self.config.read_file(open(config_path))
-        except IOError:
-            raise RuntimeError("Can't read %s " % os.path.abspath(config_path))
+    self.config = configparser.RawConfigParser()
+    try:
+      self.config.read_file(open(config_path))
+    except IOError:
+      raise RuntimeError("Can't read %s " % os.path.abspath(config_path))
 
+  def fancy_function(self):
+    """
+      #TODO Implement
 
-    def fancy_function(self):
-        """
-          #TODO Implement
+    """
 
-        """ 
+    self.logger.debug("hello wolrd")
 
-        self.logger.debug("hello wolrd")
-
-        pass
-
-
+    pass
