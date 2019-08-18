2f536b2288f8185429a33d0e9713adaaba9064eb
Werner Dreier <werner.dreier@yourscorez.de>
2019-08-18 20:26:42 +0200

    reading metadata from tsv-files

--- src/audio_korpora_pipeline/inputadapter/adapters.py
+++ src/audio_korpora_pipeline/inputadapter/adapters.py
@@ -1,5 +1,7 @@
 import os
 
+import pandas as pd
+
 from baseobjects import LoggingObject
 from metamodel.mediasession import MediaAnnotationBundle
 
@@ -37,7 +39,7 @@ class CommonVoiceAdapter(Adapter):
       raise IOError("Could not read korpus path" + korpus_path)
 
     self.audiofilenames = self._readExistingAudioFiles(korpus_path)
-    self.speakermetadata = self._readExistingSpeakerMetadta(korpus_path)
+    self.speakermetadata = self._readExistingSpeakerMetadata(korpus_path)
 
     pass
 
@@ -49,8 +51,25 @@ class CommonVoiceAdapter(Adapter):
         self.mediaAnnotationBundles.append(currentfile)
     self.logger.debug(
         "Found {} audiofiles to process".format(
-          len(self.mediaAnnotationBundles)))
+            len(self.mediaAnnotationBundles)))
     pass
 
-  def _readExistingSpeakerMetadta(self, korpus_path):
+  def _readExistingSpeakerMetadata(self, korpus_path):
+
+    existing_audio_filenames = self._getFilenamesFromMediaAnnotationBundles()
+
+    commonvoice_valid_metadatafilenames = ["dev.tsv", "test.tsv", "train.tsv",
+                                           "validated.tsv"]
+
+    combined_csv = pd.concat(
+        [pd.read_csv(os.path.join(korpus_path, f), sep="\t",
+                     header=0) for f in commonvoice_valid_metadatafilenames])
+    common_voice_valid_metadata = combined_csv[
+      combined_csv.path.isin(existing_audio_filenames)]
+
+    # TODO: map common_voice to metadata-structure
     pass
+
+  def _getFilenamesFromMediaAnnotationBundles(self):
+    return [os.path.splitext(base.identifier)[0] for base in
+            self.mediaAnnotationBundles]
