6d2ead5b157cd46f8d174ac227866836303d2d6a
Werner Dreier <werner.dreier@yourscorez.de>
2019-09-07 13:50:11 +0200

    Making windows hack only windows aware

--- src/audio_korpora_pipeline/utils.py
+++ src/audio_korpora_pipeline/utils.py
@@ -4,9 +4,24 @@ from numpy import unicode
 
 
 def winapi_path(dos_path, encoding=None):
-  if (not isinstance(dos_path, unicode) and encoding is not None):
-    dos_path = dos_path.decode(encoding)
-  path = os.path.abspath(dos_path)
-  if path.startswith(u"\\\\"):
-    return u"\\\\?\\UNC\\" + path[2:]
-  return u"\\\\?\\" + path
+  """
+  Encods a path to work also on windows if necessary, otherwise assuming unix is able to work with long filepaths
+  :param dos_path:
+  :param encoding:
+  :return:
+  """
+  given_path = dos_path
+  if (is_windows_running() == True):
+    if (not isinstance(dos_path, unicode) and encoding is not None):
+      dos_path = dos_path.decode(encoding)
+    path = os.path.abspath(dos_path)
+    if path.startswith(u"\\\\"):
+      return u"\\\\?\\UNC\\" + path[2:]
+    return u"\\\\?\\" + path
+  return given_path
+
+
+def is_windows_running():
+  if ("nt" == os.name):
+    return True
+  return False
