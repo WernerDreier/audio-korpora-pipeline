6fa903998e67c2429aa71bf5b6e2a1768c4d6d58
Werner Dreier <werner.dreier@yourscorez.de>
2019-09-15 14:45:20 +0200

    testskeleton stuff

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,21 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-    Dummy conftest.py
-
-    If you don't know what this is for, just leave it empty.
-    Read more about conftest.py under:
-    https://pytest.org/latest/plugins.html
 """
 
-# import pytest
+import pytest
+
+
+def pytest_addoption(parser):
+  parser.addoption("--input_corpora", action="store")
+  parser.addoption("--output_corpora", action="store")
+
+
+@pytest.fixture
+def input_corpora(request):
+  return request.config.getoption("--input_corpora")
+
+
+@pytest.fixture
+def output_corpora(request):
+  return request.config.getoption("--output_corpora")
--- tests/test_skeleton.py
+++ tests/test_skeleton.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import pytest
-
 def test_example():
-    assert 1 == 1
+  assert 1 == 1
+
+
+input_corpora = ["CommonVoice,CommonVoice"]
+
+
+def test_answer(input_corpora):
+  assert 0
