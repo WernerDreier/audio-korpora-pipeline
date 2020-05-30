148f1379d0530cc85beae8480e5f6183b53ce0f0
Werner Dreier <werner.dreier@yourscorez.de>
2020-05-30 13:03:12 +0200

    deleting old unittest

--- tests/audio_korpora_pipeline/inputadapter/test_adapters.py
+++ tests/audio_korpora_pipeline/inputadapter/test_adapters.py
@@ -4,7 +4,7 @@ import os
 import wave
 
 from audio_korpora_pipeline.inputadapter.adapters import UntranscribedVideoAdapter, ChJugendspracheAdapter, \
-  ArchimobAdapter, UntranscribedMediaSplittingAdapter
+  ArchimobAdapter
 from audio_korpora_pipeline.utils import load_config, config_logging
 
 
@@ -28,23 +28,6 @@ class TestUntranscribedMediaSplitterAdapter:
   def touch(self, path):
     with open(path, 'a'):
       os.utime(path, None)
-
-  def test_skipping_files_existing_and_being_named_mono(self):
-    # given
-    config = load_config("config.cfg.sample")
-    config_logging(config)
-    adapter = UntranscribedMediaSplittingAdapter(config)
-
-    # a locally existing file with ending "mono.wav" as indicator
-    filepath = "audiofiledummy.mono.wav"
-    self.touch(filepath)
-    assert os.path.exists(filepath), "dummyfile must exist to run the test"
-    # when
-    result = adapter._convertMediafileToMonoAudioThread(123, 150, filepath)
-    os.remove(filepath)  # cleaning
-    # then
-    assert result[2] == result[
-      1] == filepath, "If mono file exists, then there should not be done anything, but return positiv"  # we didnt double the mono suffix resulting in doubling files
 
 
 class TestUntranscribedVideoAdapter:
@@ -158,7 +141,7 @@ class TestChJugendspracheAdapter:
     config_logging(config)
     adapter = ChJugendspracheAdapter(config)
     # when
-    
+
     metamodel = adapter.toMetamodel()
     # then
     assert len(metamodel.mediaSessionActors) == 1, "Muss genau einen Speaker (Unknown) enthalten"
